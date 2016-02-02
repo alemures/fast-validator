421203b5be994ba7794aaec3ed14004894993eaa
alejandro <alemures@hotmail.com>
2016-02-02 16:24:20 +0000

    Improved bench.js example

--- examples/bench.js
+++ examples/bench.js
@@ -2,7 +2,14 @@
 
 var validate = require('../lib/validator').validate;
 
-// 44 ms with 100.000 times, 344 ms with 1.000.000
+var stringRules = { type:'string', min: 1, max: 10, undef: true, null: true };
+var numberRules = { type:'number', min: 1, max: 10, undef: true, null: true };
+var booleanRules = { type:'boolean', undef: true, null: true };
+var numBooleanRules = { type:'num_boolean', undef: true, null: true };
+var arrayRules = { type:'array', min: 1, max: 10, undef: true, null: true };
+var objectRules = { type:'object', min: 1, max: 10, undef: true, null: true };
+
+// 35 ms with 100.000 times, 280 ms with 1.000.000
 var TIMES = 1e+5;
 var start = Date.now();
 for (var i = 0; i < TIMES; i++) {
@@ -12,10 +19,10 @@ for (var i = 0; i < TIMES; i++) {
 console.log('Final time:', Date.now() - start, 'ms');
 
 function run() {
-  validate(['alejandro', 'santiago', 'nieto'], { type:'string', min: 1, max: 10, undef: true, null: true });
-  validate([1, 2, 3], { type:'number', min: 1, max: 10, undef: true, null: true });
-  validate([true, false, true], { type:'boolean', undef: true, null: true });
-  validate([0, 1, 1], { type:'num_boolean', undef: true, null: true });
-  validate([[1, 2, 3], ['a', 'b', 'c'], []], { type:'array', min: 1, max: 10, undef: true, null: true });
-  validate([{ a:1 }, { b:'2' }, { c:true }], { type:'object', min: 1, max: 10, undef: true, null: true });
+  validate(['alejandro', 'santiago', 'nieto'], stringRules);
+  validate([1, 2, 3], numberRules);
+  validate([true, false, true], booleanRules);
+  validate([0, 1, 1], numBooleanRules);
+  validate([[1, 2, 3], ['a', 'b', 'c'], []], arrayRules);
+  validate([{ a:1 }, { b:'2' }, { c:true }], objectRules);
 }
