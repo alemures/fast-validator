dd9dc969b8a58272e8a81aa65aa1ac9168b28684
alejandro <alemures@hotmail.com>
2016-02-12 12:30:34 +0000

    Removing _dataType from internal rules for now...

---
+++ examples/test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var fast = require('../index');
+
+// var start = Date.now();
+
+// for (var i = 0; i < 10000000; i++) {
+// }
+
+// console.log('Final time:', Date.now() - start, 'ms');
+
+
+var s = fast.string();
+
+console.log(s.validate('a'))
